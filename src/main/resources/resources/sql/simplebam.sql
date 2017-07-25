8fca440a036366be0ef4ddc05b485f6a918d58ac
Hans <Hans@HANS-PC>
2017-07-25 17:53:18 +0800

    数据

--- src/main/resources/resources/sql/simplebam.sql
+++ src/main/resources/resources/sql/simplebam.sql
@@ -125,8 +125,8 @@ CREATE TABLE `sys_order` (
 -- ----------------------------
 -- Records of sys_order
 -- ----------------------------
-INSERT INTO `sys_order` VALUES ('1', '10000', '10', '2017-07-20 14:19:20', '0', '小蒋', '1', '431333199903332455', '18620000000', '小蒋地址', '省', '省code', '市', '市code', '县', '县code', '镇', '镇code', '0', '小蒋支付人', 'zf4312222152415456', '18600000000', '8.00', '2.00', '8.00', '0.00', '200000', '333333333333', '0', '0', '0', '2017-07-23 21:42:32', '0.00', '0', '0', '0', '家宝国际', '广州电子口岸', '[Code:1800;Desc:逻辑校验通过]', '', '入库成功', '心怡物流', '已成功接收运单号', '0');
-INSERT INTO `sys_order` VALUES ('2', '20000', '20', '2017-07-20 14:19:20', '1', '爱妃', '1', '431333199903332455', '18620000000', '爱妃地址', '省', '省code', '市', '市code', '县', '县code', '镇', '镇code', '0', '爱妃支付人', 'zf4312222152415456', '18600000000', '8.00', '2.00', '8.00', '0.00', '200000', '4444444444444', '0', '0', '0', '2017-07-23 21:42:32', '0.00', '0', '0', '0', '家宝国际', '广州电子口岸', '[Code:1800;Desc:逻辑校验通过]', '', '入库成功', '心怡物流', '已成功接收运单号', '0');
+INSERT INTO `sys_order` VALUES ('1', '10000', '10', '2017-07-20 14:19:20', '0', '邹燕军', '1', '431333199903332455', '18620000000', '上海市浦东新区高行镇', '省', '省code', '市', '市code', '县', '县code', '镇', '镇code', '0', '邹燕军', 'zf4312222152415456', '18600000000', '8.00', '2.00', '8.00', '0.00', '200000', '333333333333', '0', '0', '0', '2017-07-23 21:42:32', '0.00', '0', '0', '0', '家宝国际', '广州电子口岸', '[Code:1800;Desc:逻辑校验通过]', '', '入库成功', '心怡物流', '已成功接收运单号', '0');
+INSERT INTO `sys_order` VALUES ('2', '20000', '20', '2017-07-20 14:19:20', '1', '朱琼兰', '1', '431333199903332455', '18620000000', '深圳盐田289号', '省', '省code', '市', '市code', '县', '县code', '镇', '镇code', '0', '朱琼兰', 'zf4312222152415456', '18600000000', '8.00', '2.00', '8.00', '0.00', '200000', '4444444444444', '0', '0', '0', '2017-07-23 21:42:32', '0.00', '0', '0', '0', '家宝国际', '广州电子口岸', '[Code:1800;Desc:逻辑校验通过]', '', '入库成功', '心怡物流', '已成功接收运单号', '0');
 commit;
 
 DROP TABLE IF EXISTS `sys_amountUnit`;
@@ -234,5 +234,86 @@ CREATE TABLE `sys_platform` (
   `descInfo` varchar(500) DEFAULT NULL COMMENT '描述',
   `appName` varchar(100) DEFAULT NULL COMMENT 'app名称',
   `createTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+SET FOREIGN_KEY_CHECKS=0;  
+  
+-- ----------------------------  
+-- Table structure for region  
+-- ----------------------------  
+DROP TABLE IF EXISTS `region`;  
+CREATE TABLE `region` (  
+  `Id` int(11) NOT NULL,  
+  `Name` varchar(40) default NULL,  
+  `Pid` int(11) default NULL,  
+  PRIMARY KEY  (`Id`),  
+  KEY `FK_region_REFERENCE_region` (`Pid`),  
+  CONSTRAINT `FK_region_REFERENCE_region` FOREIGN KEY (`Pid`) REFERENCES `region` (`Id`)  
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;  
+  
+-- ----------------------------  
+-- Records of region  
+-- ----------------------------  
+INSERT INTO `region` VALUES ('0', '中国', '0');  
+INSERT INTO `region` VALUES ('110000', '北京市', '0');  
+INSERT INTO `region` VALUES ('110100', '东城区', '110000');  
+INSERT INTO `region` VALUES ('110200', '西城区', '110000');  
+INSERT INTO `region` VALUES ('110500', '朝阳区', '110000');  
+INSERT INTO `region` VALUES ('110600', '丰台区', '110000');  
+INSERT INTO `region` VALUES ('110700', '石景山区', '110000');  
+INSERT INTO `region` VALUES ('110800', '海淀区', '110000');  
+INSERT INTO `region` VALUES ('110900', '门头沟区', '110000');  
+INSERT INTO `region` VALUES ('111100', '房山区', '110000');  
+INSERT INTO `region` VALUES ('111200', '通州区', '110000');  
+INSERT INTO `region` VALUES ('111300', '顺义区', '110000');  
+INSERT INTO `region` VALUES ('111400', '昌平区', '110000');  
+INSERT INTO `region` VALUES ('111500', '大兴区', '110000');  
+INSERT INTO `region` VALUES ('111600', '怀柔区', '110000');  
+INSERT INTO `region` VALUES ('111700', '平谷区', '110000');  
+INSERT INTO `region` VALUES ('112800', '密云县', '110000');  
+INSERT INTO `region` VALUES ('112900', '延庆县', '110000');  
+INSERT INTO `region` VALUES ('120000', '天津市', '0');  
+INSERT INTO `region` VALUES ('120100', '和平区', '120000');  
+INSERT INTO `region` VALUES ('120200', '河东区', '120000');  
+INSERT INTO `region` VALUES ('120300', '河西区', '120000');  
+INSERT INTO `region` VALUES ('120400', '南开区', '120000');  
+INSERT INTO `region` VALUES ('120500', '河北区', '120000');  
+INSERT INTO `region` VALUES ('120600', '红桥区', '120000');  
+INSERT INTO `region` VALUES ('120900', '滨海新区', '120000');  
+INSERT INTO `region` VALUES ('121000', '东丽区', '120000');  
+INSERT INTO `region` VALUES ('121100', '西青区', '120000');  
+INSERT INTO `region` VALUES ('121200', '津南区', '120000');  
+INSERT INTO `region` VALUES ('121300', '北辰区', '120000');  
+INSERT INTO `region` VALUES ('121400', '武清区', '120000');  
+INSERT INTO `region` VALUES ('121500', '宝坻区', '120000');  
+INSERT INTO `region` VALUES ('122100', '宁河县', '120000');  
+INSERT INTO `region` VALUES ('122300', '静海县', '120000');  
+INSERT INTO `region` VALUES ('122500', '蓟县', '120000');  
+INSERT INTO `region` VALUES ('130000', '河北省', '0');  
+INSERT INTO `region` VALUES ('130100', '石家庄市', '130000');  
+INSERT INTO `region` VALUES ('130101', '市辖区', '130100');  
+INSERT INTO `region` VALUES ('130102', '长安区', '130101');  
+INSERT INTO `region` VALUES ('130103', '桥东区', '130101');  
+INSERT INTO `region` VALUES ('130104', '桥西区', '130101');  
+INSERT INTO `region` VALUES ('130105', '新华区', '130101');  
+INSERT INTO `region` VALUES ('130107', '井陉矿区', '130101');  
+COMMIT;
+
+
+SET FOREIGN_KEY_CHECKS=0;  
+  
+-- ----------------------------  
+-- Table structure for region  
+-- ----------------------------  
+DROP TABLE IF EXISTS `sys_orderDetail`;  
+CREATE TABLE `sys_orderDetail` (
+  `id` bigint(20) NOT NULL,
+  `orderId` bigint(20) DEFAULT NULL COMMENT '订单Id',
+  `productId` bigint(20) DEFAULT NULL COMMENT '商品Id',
+  `amount` int(5) DEFAULT NULL COMMENT '数量',
+  `prices` double(12,2) DEFAULT NULL COMMENT '价格',
+  `totalMoney` double(12,2) DEFAULT NULL COMMENT '总价',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
